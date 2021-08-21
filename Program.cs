e549be47856cd4d316fc09ebc8bf0a0b7402c7d8
Vinicius Lopes <vinicioslop7@hotmail.com>
2021-08-20 22:40:00 -0300

    Salva metade do programa em construção

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            string opiniao;
+
+            Console.WriteLine("Usuário, qual sua opinião sobre mim? Estou curioso.");
+            Console.WriteLine();
+
+            Console.Write("Digite sua opinião: ");
+            opiniao = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Interessante seu ponto, mas sabe quem se importa?");
+            Console.ReadKey();
+            Console.WriteLine("Eu mesmo, achou que ia falar outra coisa?");
+        }
+    }
+}
